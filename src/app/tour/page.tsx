5606e1d61f1d8f23dc3cae491153860c2079b474
pllobkhalil191 <pollob@triumphdigital.co.th>
2024-12-14 11:56:22 +0600

    issue solve

--- src/app/tour/page.tsx
+++ src/app/tour/page.tsx
@@ -1,28 +1,31 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TourCard from "@/components/TourCard";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const TourPage = () => {
   const [locationId, setLocationId] = useState<number | null>(null); // Define state for locationId
   const searchParams = useSearchParams();
-  const queryLocationId = searchParams.get("location_id");
+  const queryLocationId = searchParams?.get("location_id");
 
   const parsedLocationId = queryLocationId ? parseInt(queryLocationId, 10) : null;
-  
+
   // Set the locationId from the query string if available
-  React.useEffect(() => {
+  useEffect(() => {
     if (parsedLocationId !== null) {
       setLocationId(parsedLocationId);
     }
   }, [parsedLocationId]);
 
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       {/* Pass locationId and setLocationId to the TourCard component */}
-      <TourCard locationId={locationId} setLocationId={setLocationId} />
-    </div>
+      <div>
+        <TourCard locationId={locationId} setLocationId={setLocationId} />
+      </div>
+    </Suspense>
   );
 };
 
